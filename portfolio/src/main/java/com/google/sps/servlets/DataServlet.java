b2f8b2ca4f1f4ec984d4b7b69ceabf9828d18c5c
RCAVelez <chve38@gmail.com>
2020-06-06 00:03:10 -0500

    Resolve conflicts1

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -39,7 +39,6 @@ public class DataServlet extends HttpServlet {
 
     response.setContentType("application/json");
     response.getWriter().println(json);
-    response.sendRedirect("/index.html");
   }
 
   @Override
